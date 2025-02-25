c63bb4011b54c1fcdedcc3db7c95b16fd57ad548
surfer971 <57986206+surfer971@users.noreply.github.com>
2025-02-25 19:38:21 +0200

    "First commit"

---
+++ src/data/data_prep.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
---
+++ src/dataset.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+
+a = 12
+b = 12
+
+print(add(a, b))
